4e014eaa8bc05fcb5964bb7fa4338ddd65ed7e10
Jedediah Hernandez <Jedediah@me.com>
2019-02-19 23:11:44 -0700

    fixing heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require bootstrap
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
