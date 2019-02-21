2da3537a78e7e560c3f814df7a2d73ab0cc0c37d
Jedediah Hernandez <Jedediah@me.com>
2019-02-21 15:59:18 -0700

    trying to fix heroku with bootstrap drop down menu.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
