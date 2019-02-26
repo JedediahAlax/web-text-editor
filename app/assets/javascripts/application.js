bdc3034fe08bcc9af5a3a6a4d6a21ed2aeaf3899
Jedediah Hernandez <Jedediah@me.com>
2019-02-26 15:15:36 -0700

    trying to fix heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// require bootstrap
+// require bootstrap/dropdown
 //= require jquery
 //= require rails-ujs
 //= require activestorage
