707171c8c82d00eb0112f21a923f8343d73c39b1
Jedediah Hernandez <Jedediah@me.com>
2019-02-28 15:25:29 -0700

    heroku not working at all

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.serve_static_assets = true
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
