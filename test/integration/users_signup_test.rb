e3cb18235c7fc6377d8ef5a6e3302d466d8c827e
Jedediah Hernandez <Jedediah@me.com>
2019-04-13 22:21:30 -0600

    fixing heroku

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-<<<<<<< HEAD
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -17,23 +16,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
 
   test "valid signup information" do
-=======
-  def setup
-    ActionMailer::Base.deliveries.clear
-  end
-
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: " ", email: "user@invalid",
-                                          password:  "foo",
-                                          password_confirmation: "bar" } }
-    end
-    assert_template 'users/new'
-  end
-
-  test "valid signup information with account activation" do
->>>>>>> account_activation
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
