16dd173c1472a93f9119d65651a5222f7ff5dac7
Jedediah Hernandez <jedediah@me.com>
2019-04-13 22:19:43 -0600

    Update user_activation_controller.rb
    removing ===== and >>>>>> inserted by atom

--- app/controllers/user_activation_controller.rb
+++ app/controllers/user_activation_controller.rb
@@ -5,12 +5,12 @@ class UserActivationController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-<<<<<<< HEAD
+
       user.update_attribute(:activated, true)
       user.update_attribute(:activated_at, Time.zone.now)
-=======
+
       user.activate
->>>>>>> account_activation
+      account_activation
       log_in user
       flash[:success] = "Your Textitor account has been activated!"
       redirect_to user
