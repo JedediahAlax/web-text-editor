2d311d0623d1c46d0f8d39874846a7bbdc7b1885
Jedediah Hernandez <Jedediah@me.com>
2019-04-13 23:09:52 -0600

    trying to send activation emails through heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,17 +71,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://web-text-editor.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'https://web-text-editor.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   ActionMailer::Base.smtp_settings = {
+    :from => 'textitoronline@gmail.com',
     :address      => 'smtp.sendgrid.net',
     :port         => '587',
     :authentication => :plain,
     :user_name    => ENV['SENDGRID_USERNAME'],
     :password     => ENV['SENDGRID_PASSWORD'],
-    :domain       => 'heroku.com',
+    :domain       => 'https://web-text-editor.herokuapp.com',
     :enable_starttls_auto => true
   }
 
